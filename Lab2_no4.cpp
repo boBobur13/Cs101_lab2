0bc34cafed903c76c7d4454ba8384f8df8ff1d5f
Brain17 <b.daminov@newuu.uz>
2025-06-09 00:34:56 +0500

    Add files via upload

---
+++ Lab2_no4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cin>> a>>b;
+    if (a%b==0) {
+            cout<< a << " is divisible by " << b<< endl;
+    } else {
+        cout << a << " is not divisible by " << b << endl;
+    }
+    return 0;
+}
